d5c84a5c04b5168c355b2e982e1f3350b62bc9a8
caua-ananias <caua.ananias2004@gmail.com>
2025-02-17 21:04:55 -0400

    user insertion with post

--- src/main/java/com/course/workshopmongo/services/UserService.java
+++ src/main/java/com/course/workshopmongo/services/UserService.java
@@ -1,10 +1,12 @@
 package com.course.workshopmongo.services;
 
 import com.course.workshopmongo.domain.User;
+import com.course.workshopmongo.dto.UserDTO;
 import com.course.workshopmongo.repository.UserRepository;
 import com.course.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 
@@ -21,6 +23,14 @@ public class UserService {
     public User findById(String id) {
         User user = repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("User not found"));
         return user;
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 
 
