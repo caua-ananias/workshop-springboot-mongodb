39302a3fd3505b3bbcc8751ca0533bc0351318ac
caua-ananias <caua.ananias2004@gmail.com>
2025-02-17 22:50:22 -0400

    Simple query with @Query

--- src/main/java/com/course/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/course/workshopmongo/repository/PostRepository.java
@@ -2,10 +2,14 @@ package com.course.workshopmongo.repository;
 
 import com.course.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{'title': { $regex:  ?0, $options: 'i' } }")
+    List<Post> findByTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
