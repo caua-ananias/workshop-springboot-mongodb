c513655cec1619aed594bebf6a4993a3c88c30b3
caua-ananias <caua.ananias2004@gmail.com>
2025-02-17 22:44:34 -0400

    Simple query with query methods

--- src/main/java/com/course/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/course/workshopmongo/repository/PostRepository.java
@@ -3,5 +3,9 @@ package com.course.workshopmongo.repository;
 import com.course.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
